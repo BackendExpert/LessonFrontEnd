68c0e881bf961b313fad32186e00646203fe434c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-10-11 18:59:34 +0530

    Update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,13 +11,15 @@ const HomePage = () => {
             <div className=""></div>
         </div> */}
 
-        <div className="flex justify-between py-[5%]">
+        <div className="md:mx-0 mx-4 md:flex justify-between py-[5%]">
             <div className="w-1/2"></div>
             <div className="bg-white rounded-md p-4 shadow-md w-full">
                 <h1 className='text-center text-2xl font-semibold text-gray-500'>Lesson Plan Genarator</h1>
             </div>
             <div className="w-1/2"></div>
         </div>
+        
+        
     </div>
   )
 }
