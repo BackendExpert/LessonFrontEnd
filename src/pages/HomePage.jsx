66a35deaa0b4ae50dc3968d6b140bd65026bbd7a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-10-11 15:21:05 +0530

    Update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className='bg-gray-200 min-h-screen'>
+        {/* <div className="md:grid grid-cols-3 gap-4 py-[5%]">
+            <div className=""></div>
+            <div className="bg-white rounded-md p-4 shadow-md">
+
+            </div>
+            <div className=""></div>
+        </div> */}
+
+        <div className="flex justify-between py-[5%]">
+            <div className="w-1/2"></div>
+            <div className="bg-white rounded-md p-4 shadow-md w-full">
+                <h1 className='text-center text-2xl font-semibold text-gray-500'>Lesson Plan Genarator</h1>
+            </div>
+            <div className="w-1/2"></div>
+        </div>
+    </div>
   )
 }
 
