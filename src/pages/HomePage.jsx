94e408ac8e5a26b06ec78e22eec28b6f85079563
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-10-12 13:54:09 +0530

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const HomePage = () => {
     const [file, setFile] = useState(null);
@@ -43,7 +43,27 @@ const HomePage = () => {
         catch(err){
             console.log(err)
         }
+    }
+    // get all lessons
+    useEffect(() => {
+      axios.get(import.meta.env.VITE_APP_API + '/FileUpload/AllLessons')
+      .then(res => setLessons(res.data.Result))
+      .catch(err => console.log(err))
+    }, [])
 
+    const [LessonPlan, SetLessonPlan] = useState({
+      lessonsPlan: ''
+    })
+
+    const headleCreateLessonPlan = (e) =>{
+        e.preventDefault()
+
+        try{
+          console.log(LessonPlan)
+        }
+        catch(err){
+          console.log(err)
+        }
     }
 
   return (
@@ -83,7 +103,23 @@ const HomePage = () => {
                         
                     </div>
                 </div>
-                <div className="bg-white rounded-md p-4 shadow-md w-full md:ml-2 md:mt-0 mt-4"></div>
+                <div className="bg-white rounded-md p-4 shadow-md w-full md:ml-2 md:mt-0 mt-4">
+                    <form onSubmit={headleCreateLessonPlan} method="post">
+                      <h1 className="font-semibold text-gray-500 mb-2">Select Lesson to Create Lesson Plan</h1>
+                      <select name="" id="" className='w-full bg-gray-200 rounded pl-2 h-12' required onChange={SetLessonPlan({...LessonPlan, lessonsPlan:e.target.value})}>
+                          <option value="">Select Lesson</option>                          
+                          {
+                            lessons.map((lesson, index) => {
+                              return(
+                                <option key={index} value={lesson._id}>{lesson.title}</option>
+                              )
+                            })
+                          }
+                      </select>
+
+                      <button type="submit" className='bg-blue-500 rounded w-full text-white py-2 px-4 mt-8'>Create Lesson Plan</button>
+                    </form>
+                </div>
               </dlv>
             </div>
             <div className="w-1/2"></div>
